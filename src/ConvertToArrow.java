99251a1c8ad7f0f1acfccf249f28b8f8f410da05
Juan <juanf.tonina@gmail.com>
2016-06-09 15:07:16 -0300

    Optimizing imports

--- src/ConvertToArrow.java
+++ src/ConvertToArrow.java
@@ -7,17 +7,11 @@ import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.fileTypes.LanguageFileType;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.ui.Messages;
-import com.intellij.openapi.ui.popup.Balloon;
-import com.intellij.openapi.ui.popup.JBPopupFactory;
 import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.openapi.wm.StatusBar;
-import com.intellij.openapi.wm.WindowManager;
-import com.intellij.psi.*;
-import com.intellij.refactoring.RefactoringFactory;
-import com.intellij.ui.JBColor;
-import com.intellij.ui.awt.RelativePoint;
-import com.jgoodies.common.base.Objects;
+import com.intellij.psi.PsiDocumentManager;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.PsiFileFactory;
 
 public class ConvertToArrow extends AnAction {
     public ConvertToArrow() {
