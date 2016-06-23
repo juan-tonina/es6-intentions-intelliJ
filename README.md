10ce7282cecfbb5305db9efd3a9188dded14b132
Juan <juanf.tonina@gmail.com>
2016-06-23 14:22:43 -0300

    0.2.1, added "Convert to template string" and stuff

--- README.md
+++ README.md
@@ -1,10 +1,14 @@
 # es6-intentions-intelliJ
 Proof of concept, testing jetbrains plugin development
 
-Version 0.1
+Version 0.2.1
 
-This plugin adds two refactor options for: convert to/from arrow function. 
-It performs a few validations and only takes care of the sintax change, version 0.2 will manage "this" and "arguments" keywords (if possible).
+This plugin adds refactor options for:
+ - Convert to/from arrow function.(It performs a few validations and only takes care of the syntax change.)
+ - Convert to template string (buggy when there are operators without parenthesis: be sure to double check the outcome)
+
+If converting to template string does not work as intended, you can add parenthesis and its content will be escaped
+with ${}.
 
 Use at your own risk! 
 Possible outcomes include: broken code, stupid code, fire, death.
--- src/CustomEs6Intentions.java
+++ src/CustomEs6Intentions.java
@@ -14,10 +14,12 @@ public class CustomEs6Intentions implements ApplicationComponent {
         ActionManager am = ActionManager.getInstance();
         ConvertFromArrow action = new ConvertFromArrow();
         ConvertToArrow action2 = new ConvertToArrow();
+        ConvertToTemplateString action3 = new ConvertToTemplateString();
 
         // Passes an instance of your custom ConvertFromArrow class to the registerAction method of the ActionManager class.
         am.registerAction("Convert from arrow function", action);
         am.registerAction("Convert to arrow function", action2);
+        am.registerAction("Convert to template string", action3);
 
         // Gets an instance of the WindowMenu action group.
         DefaultActionGroup jsHierarchyPopUpMenu = (DefaultActionGroup) am.getAction("RefactoringMenu");
@@ -26,6 +28,8 @@ public class CustomEs6Intentions implements ApplicationComponent {
         jsHierarchyPopUpMenu.addSeparator();
         jsHierarchyPopUpMenu.add(action);
         jsHierarchyPopUpMenu.add(action2);
+        jsHierarchyPopUpMenu.addSeparator();
+        jsHierarchyPopUpMenu.add(action3);
     }
 
     // Disposes system resources.
